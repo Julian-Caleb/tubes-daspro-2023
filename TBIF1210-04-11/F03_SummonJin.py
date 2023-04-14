89d17ec6b9de76c775e3645c6c524195680dc186
RafidFadhila2308 <130205392+RafidFadhila2308@users.noreply.github.com>
2023-04-14 22:12:33 +0700

    Update F03_SummonJin.py

--- TBIF1210-04-11/F03_SummonJin.py
+++ TBIF1210-04-11/F03_SummonJin.py
@@ -1,37 +1,36 @@
-def loginJin(x):
+ddef loginJin(x):
     usernameJin = ["" for i in range(x)]
     passwordJin = ["" for i in range(x)]
     z = 0
     while(z < x):
         usernameJin = str(input("Masukkan username jin: "))
         passwordJin = str(input("Masukkan password jin: "))
-        if(usernameJin[z] == ""): # belum lengkap kalau username sama
+        if(usernameJin[z] == ""): # belum lengkap buat condition username jin sama
             print("Username", usernameJin, "sudah diambil!")
         else:
-            if(len(passwordJin) < 5 or len(passwordJin) > 25):
+            if(len(passwordJin) < 5 or len(passwordJin) > 25): # ketentuan password
                 print("Password panjangnya harus 5-25 karakter!")
             else:
-                z = z + 1
+                z = z + 1 # lanjut ke input selanjutnya kalau password udah bener
                 print("Mengumpulkan sesajen…")
                 print("Menyerahkan sesajen…")
                 print("Membacakan ")
                 print("Jin", usernameJin, "berhasil dipanggil!")
 
-    for i in range (x): # Buat nyetak list
+    for i in range (x): # buat nyetak list (gk termasuk ke spek, cmn buat liat aja kalau username & password bener jadi list)
         print(usernameJin, passwordJin)
-
 
 def summonJin(y, username):
     Nmax = 103
     if(username != "bandung_bondowoso"):
         print("") # pesan kalau bukan bandung_bondowoso apa?
     else:
-        if(y == Nmax):
+        if(y == Nmax): # kalau jin udh maksimal?
             print("Jumlah Jin telah maksimal! (100 jin). Bandung tidak dapat men-summon lebih dari itu")
         else:
             print("Jenis jin yang dapat dipanggil:")
             print("(1) Pengumpul - Bertugas mengumpulkan bahan bangunan")
-            ("(2) Pembangun - Bertugas membangun candi")
+            print("(2) Pembangun - Bertugas membangun candi")
         for i in range(y):
             nomor = int(input("Masukkan nomor jenis jin yang ingin dipanggil: "))
             if(nomor == 1):
@@ -46,6 +45,18 @@ def summonJin(y, username):
                 print("Tidak ada jenis jin bernomor", nomor, "!")
 
 
+                
+                
+# INI BUAT KALAU USERNAME SAMA, CUMAN BELUM JALAN. PAS DI RUN MASIH OUT OF INDEX (line 57)
+x = 3 # panjang list
+a = [0 for i in range(x)]
+j = 0
+while(j < x): # loop input
+    a = str(input("Masukkan username jin: "))
+    for k in range (j-1):
+        if(a[j] == a[k]): # nilai input sekarang dibandingkan dengan nilai input sebelumnya, hingga index sebelum index input sekarang (-1)
+            print("sama")
+    j = j + 1
 
 
         
